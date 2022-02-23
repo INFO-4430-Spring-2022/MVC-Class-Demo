24b13be37ddc88ab2843c4bb1bf563f9b1adf6b8
Jon Holmes <holmjona@isu.edu>
2022-02-23 14:51:08 -0700

    Started the controllers discussion.

---
+++ MVCDemo/Controllers/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.Controllers {
+    public class PersonController : Controller {
+        public IActionResult Index() {
+            //ViewBag.Pizza = "Pepperoni";
+            //ViewData["IceCream"] = "Chocolate";
+            return View();
+        }
+
+        public IActionResult Details() {
+            TempData["Pizza"] = "Meat Lovers";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
