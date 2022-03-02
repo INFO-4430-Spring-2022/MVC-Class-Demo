fe84c6bf16bb1e628113043069225be962b49ae3
Jon Holmes <holmjona@isu.edu>
2022-03-02 14:48:17 -0700

    More about views Specifically, View Templates and POST v GET Actions.

--- MVCDemo/Controllers/PersonController.cs
+++ MVCDemo/Controllers/PersonController.cs
@@ -6,19 +6,66 @@ namespace MVCDemo.Controllers {
         public IActionResult Index() {
             //ViewBag.Pizza = "Pepperoni";
             //ViewData["IceCream"] = "Chocolate";
-            return View();
+            return View("List", GetPeople());
+        }
+
+        public List<Person> GetPeople() {
+            List<Person> lst = new List<Person>();
+            lst.Add(getBob());
+            lst.Add(new Person() { FirstName = "Sally", LastName = "Smith" });
+            lst.Add(new Person() { FirstName = "Jerry", LastName = "Gerison" });
+            lst.Add(new Person() { FirstName = "Gus", LastName = "Guster" });
+
+            return lst;
+        }
+
+
+        public Person getBob() {
+            Person p = new Person();
+            p.FirstName = "Bob";
+            p.LastName = "Awesome";
+            p.Prefix = "Mr.";
+            p.Postfix = "Esquire";
+            p.Homepage = "http://www.iamawesome.com";
+            p.Phone = "5555551234";
+            p.Email = "bob@iamawesome.com";
+            return p;
         }
 
         public IActionResult Details() {
             //TempData["Pizza"] = "Meat Lovers";
             //return RedirectToAction("Index");
-            Person p = new Person();
-            p.FirstName = "Bob";
-            p.LastName = "Awesome";
+
+            Person bob = getBob();
             ThingType tType = new ThingType();
             tType.Name = "thing one";
-            return View(p);
+            return View("About", bob);
+
         }
+
+        public IActionResult Delete() {
+
+            Person bob = getBob();
+            return View(bob);
+        }
+
+        [HttpGet]
+        public IActionResult Edit() {
+            return View(getBob());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id,
+            string firstName) {
+            return View(getBob());
+        }
+
+        public IActionResult Create() {
+            Person newP = new Person();
+            newP.DateOfBirth = DateTime.Now;
+            return View(newP);
+        }
+
 
     }
 }
